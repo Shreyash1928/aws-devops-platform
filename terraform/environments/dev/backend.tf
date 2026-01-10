f2d5006a1ee2f5b5663d1d01a540e2fb9def8220
Shreyash1928 <shreyashwadkar1991@gmail.com>
2026-01-10 19:45:51 +0000

    Add S3 remote backend and CI pipeline

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "shreyash-terraform-state"
+    key            = "aws-devops-platform/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
