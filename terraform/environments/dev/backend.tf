bd54d7d7a880eb5d14963c06ff6b4893c9ccc5cc
Shreyash1928 <shreyashwadkar1991@gmail.com>
2026-01-13 19:28:28 +0000

    Reconfigure Terraform backend for new AWS account

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "shreyash-terraform-state"
+    bucket         = "shreyash-terraform-state-1928"
     key            = "aws-devops-platform/dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
